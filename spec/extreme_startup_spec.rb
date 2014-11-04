3289468949d1a4271257f59d82f38fdeedf1013c
marcwinstanley <marc.winstanley@onthebeach.co.uk>
2014-11-04 11:55:07 +0000

    Answering first question

--- spec/extreme_startup_spec.rb
+++ spec/extreme_startup_spec.rb
@@ -2,24 +2,24 @@ require 'spec_helper'
 
 describe Question do
 
-  it 'takes a string' do
-    Question.new('question string')
-  end
-
-  it 'answers' do
-    expect(Question.new('question').answer)
-  end
-
   describe 'answering questions' do
     subject { Question.new(question).answer }
 
-    context 'what is x plus y' do
+    context 'what is 6 plus 19' do
       let(:question) { 'f9380cd0: what is 6 plus 19' }
 
       it 'returns 25' do
         expect(subject).to eq 25
       end
 
+    end
+
+    context 'what is x plus y' do
+      let(:question) { 'f9380cd0: what is 6 plus 20' }
+
+      it 'returns 25' do
+        expect(subject).to eq 26
+      end
 
     end
   end 
--- startup.rb
+++ startup.rb
@@ -6,11 +6,14 @@ set :bind, '0.0.0.0'
 class Question
 
   def initialize(param_string)
-
+    @param_string = param_string
   end
 
   def answer
-    25
+    params = @param_string.split(":")
+    question = params[1]
+    answers = question.match(/what is (\d+) plus (\d+)/).captures
+    answer = answers[0].to_i + answers[1].to_i
   end
 
 end
